df90b102e745bc9dc72480e639f5374c38dbae2c
murphyad <murphyad@msoe.edu>
2019-02-10 15:57:20 -0600

    Began implementing location services integration into main activity

--- SARMobileClient/app/src/main/java/se3810/groupi/MainActivity.kt
+++ SARMobileClient/app/src/main/java/se3810/groupi/MainActivity.kt
@@ -1,17 +1,33 @@
+/*
+ * The home screen of the SAR app.  This contains a list of nearby tags, and provides a button
+ * to create a new tag at the current location of the user.
+ */
+
 package se3810.groupi
 
 import android.content.Intent
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
-    var nearbyTags : NearbyTags = NearbyTags()
+    var nearbyTags : NearbyTags? = null
+    private var locationManager : LocationManager? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initialize NearbyTags
+        this.nearbyTags = NearbyTags()
+
+        //Initialize location services
+        this.locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+
+
         loadTags()
     }
 
@@ -21,6 +37,8 @@ class MainActivity : AppCompatActivity() {
     fun createTag(view: View){
         // Create an Intent to start the second activity
         val tagCreateIntent = Intent(this, TagCreationScreen::class.java)
+
+        //Send coordinates
 
         // Start the new activity.
         startActivity(tagCreateIntent)
@@ -37,5 +55,9 @@ class MainActivity : AppCompatActivity() {
          * 3) format results into a list of tags
          * 4) call nearbyTags.reload(new list of tags)
          */
+    }
+
+    fun getLocation() {
+
     }
 }
