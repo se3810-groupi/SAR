d9c1608c4a79b020454b0f8c6bfeb476a471066c
murphyad <murphyad@msoe.edu>
2019-02-11 20:40:32 -0600

    User's location is now updated every 1 second and stored.

--- SARMobileClient/app/src/main/java/se3810/groupi/MainActivity.kt
+++ SARMobileClient/app/src/main/java/se3810/groupi/MainActivity.kt
@@ -8,38 +8,31 @@
 
 package se3810.groupi
 
-import android.Manifest.permission.ACCESS_COARSE_LOCATION
-import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.Manifest
 import android.annotation.SuppressLint
-import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.support.design.widget.Snackbar
-import android.support.design.widget.Snackbar.LENGTH_INDEFINITE
+import android.os.Looper
 import android.support.v4.app.ActivityCompat
-import android.util.Log
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import android.widget.Toast
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import se3810.groupi.BuildConfig.APPLICATION_ID
+import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var nearbyTags : NearbyTags
     private lateinit var location : Location
     private lateinit var tagTable : TableLayout
-    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
-
-    // Setup Fused Location Provider entry point
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var mLocationRequest : LocationRequest
+    private val UPDATE_INTERVAL : Long = 1000 // 1 sec
+    private val FASTEST_INTERVAL : Long = 500 // .5 sec
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,24 +44,13 @@ class MainActivity : AppCompatActivity() {
         //Initialize NearbyTags
         this.nearbyTags = NearbyTags()
 
-        //Initialize location services
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
         //Temp values that will be overwritten by getLastLocation onStart
         location = Location(-1.0,-1.0,-1.0)
 
-        loadTags()
-    }
+        //Get new location every 1 sec
+        startLocationUpdates()
 
-    override fun onStart() {
-        super.onStart()
-        if(!checkPermissions()) {
-            println("requesting permissions")
-            requestPermissions()
-        }else{
-            println("getting location")
-            getLastLocation()
-        }
+        loadTags()
     }
 
     /**
@@ -152,116 +134,71 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Source: https://github.com/googlesamples/android-play-location/blob/master/BasicLocationSample/kotlin/
-    @SuppressLint("MissingPermission")
-    private fun getLastLocation() {
-        fusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
-            if(task.isSuccessful && task.result != null){
-                location.latitude = task.result!!.latitude
-                location.longitude = task.result!!.longitude
-                location.altitude = task.result!!.altitude
+    //Adapted from https://github.com/codepath/android_guides/wiki/Retrieving-Location-with-LocationServices-API and
+    //https://github.com/devangakhani/GPSLocation
+    fun startLocationUpdates() {
+        this.mLocationRequest = LocationRequest()
+        this.mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        this.mLocationRequest.interval = UPDATE_INTERVAL
+        this.mLocationRequest.fastestInterval = FASTEST_INTERVAL
 
-                println(location.latitude)
-            }else{
-                Log.w("MainActivity", "getLastLocation:exception", task.exception)
-                println("No location detected")
+        // Create LocationSettingRequest object
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(mLocationRequest)
+        val locationSettingsRequest = builder.build()
+
+        //Initialize service object
+        val settingsClient = LocationServices.getSettingsClient(this)
+        settingsClient!!.checkLocationSettings(locationSettingsRequest)
+
+        registerLocationListener()
+    }
+
+    @SuppressLint("ObsoleteSdkInt")
+    private fun registerLocationListener() {
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                onLocationChanged(locationResult!!.lastLocation)
             }
         }
+        // add permission if android version is greater then 23
+        if(Build.VERSION.SDK_INT >= 23 && checkPermission()) {
+            LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, locationCallback, Looper.myLooper())
+        }
+    }
+    //Source: https://github.com/devangakhani/GPSLocation
+    private fun onLocationChanged(location: android.location.Location) {
+        // create message for toast with updated latitude and longitude
+        val msg = "Updated Location: " + location.latitude  + " , " + location.longitude + " , " + location.altitude
+        println(msg)
+
+        this.location.latitude = location.latitude
+        this.location.longitude = location.longitude
+        this.location.altitude = location.altitude
     }
 
-    // Source: https://github.com/googlesamples/android-play-location/blob/master/BasicLocationSample/kotlin/
-    private fun checkPermissions() : Boolean{
-        return ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED
-    }
-
-    // Source: https://github.com/googlesamples/android-play-location/blob/master/BasicLocationSample/kotlin/
-    private fun startLocationPermissionRequest() {
-        ActivityCompat.requestPermissions(this, arrayOf(ACCESS_COARSE_LOCATION),
-            REQUEST_PERMISSIONS_REQUEST_CODE)
-    }
-
-    // Source: https://github.com/googlesamples/android-play-location/blob/master/BasicLocationSample/kotlin/
-    private fun requestPermissions() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_COARSE_LOCATION)) {
-            // Provide an additional rationale to the user. This would happen if the user denied the
-            // request previously, but didn't check the "Don't ask again" checkbox.
-            Log.i("MainActivity", "Displaying permission rationale to provide additional context.")
-            startLocationPermissionRequest()
-
+    //Source: https://github.com/devangakhani/GPSLocation
+    private fun checkPermission() : Boolean {
+        if (ContextCompat.checkSelfPermission(this , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true
         } else {
-            // Request permission. It's possible this can be auto answered if device policy
-            // sets the permission in a given state or the user denied the permission
-            // previously and checked "Never ask again".
-            Log.i("MainActivity", "Requesting permission")
-            startLocationPermissionRequest()
+            requestPermissions()
+            return false
         }
     }
 
-    // Source: https://github.com/googlesamples/android-play-location/blob/master/BasicLocationSample/kotlin/
-    /**
-     * Shows a [Snackbar].
-     *
-     * @param snackStrId The id for the string resource for the Snackbar text.
-     * @param actionStrId The text of the action item.
-     * @param listener The listener associated with the Snackbar action.
-     */
-    private fun showSnackbar(
-        snackStrId: Int,
-        actionStrId: Int = 0,
-        listener: View.OnClickListener? = null
-    ) {
-        val snackbar = Snackbar.make(findViewById(android.R.id.content), getString(snackStrId),
-            LENGTH_INDEFINITE)
-        if (actionStrId != 0 && listener != null) {
-            snackbar.setAction(getString(actionStrId), listener)
-        }
-        snackbar.show()
+    //Source: https://github.com/devangakhani/GPSLocation
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(this, arrayOf("Manifest.permission.ACCESS_FINE_LOCATION"),1)
     }
 
-    // Source: https://github.com/googlesamples/android-play-location/blob/master/BasicLocationSample/kotlin/
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        Log.i("MainActivity", "onRequestPermissionResult")
-        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
-            when {
-                // If user interaction was interrupted, the permission request is cancelled and you
-                // receive empty arrays.
-                grantResults.isEmpty() -> Log.i("MainActivity", "User interaction was cancelled.")
-
-                // Permission granted.
-                (grantResults[0] == PackageManager.PERMISSION_GRANTED) -> getLastLocation()
-
-                // Permission denied.
-
-                // Notify the user via a SnackBar that they have rejected a core permission for the
-                // app, which makes the Activity useless. In a real app, core permissions would
-                // typically be best requested during a welcome-screen flow.
-
-                // Additionally, it is important to remember that a permission might have been
-                // rejected without asking the user for permission (device policy or "Never ask
-                // again" prompts). Therefore, a user interface affordance is typically implemented
-                // when permissions are denied. Otherwise, your app could appear unresponsive to
-                // touches or interactions which have required permissions.
-                else -> {
-                    showSnackbar(R.string.permission_denied_explanation, R.string.settings,
-                        View.OnClickListener {
-                            // Build intent that displays the App settings screen.
-                            val intent = Intent().apply {
-                                action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                                data = Uri.fromParts("package", APPLICATION_ID, null)
-                                flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                            }
-                            startActivity(intent)
-                        })
-                }
+    //Source: https://github.com/devangakhani/GPSLocation
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 1) {
+            if (permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION ) {
+                registerLocationListener()
             }
         }
     }
-
 }
